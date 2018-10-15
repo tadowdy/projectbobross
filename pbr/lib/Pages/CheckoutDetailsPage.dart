83a9b8af3c693b2c551a75a3e96d9d28c6792503
Clifford <thomasmclifford96@gmail.com>
2018-10-15 18:17:23 -0500

    - Add detail pages for push navigation. - Add push navigation support for survey on home page. - Change file organization to separate pages from body widgets. - Change file names and class names to match and follow camelCaseConvention.

---
+++ pbr/lib/Pages/CheckoutDetailsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckoutDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+          title: new Text('Checkout Details')
+      ),
+      body: Center(
+        child: Text('Put the Checkout Details Here..'),
+      ),
+    );
+  }
+}
