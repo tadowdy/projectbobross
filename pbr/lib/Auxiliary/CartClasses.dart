b99d406ab8712d3d56fbd40e51337d418d7a3b9e
ZFenton <zfenton@siue.edu>
2018-10-17 19:14:33 -0500

    integrate cart wireframe changes into bottom navigation; refactored cart files

---
+++ pbr/lib/Auxiliary/CartClasses.dart
@@ -0,0 +1,12 @@
+//TODO: make ItemInfo instead
+class ArtInfo {
+  String artworkName;
+  String artistName;
+  double price;
+
+  ArtInfo(String artworkName, String artistName, double price) {
+    this.artworkName = artworkName;
+    this.artistName = artistName;
+    this.price = price;
+  }
+}
--- pbr/lib/uiComponents.dart
+++ pbr/lib/uiComponents.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// TODO: move to auxiliary
 final topAppBar = new AppBar(
           title: new Image(image: new AssetImage('images/full_logo.png')),
           actions: <Widget>[
@@ -8,30 +9,6 @@ final topAppBar = new AppBar(
               tooltip: 'QR scan',
               onPressed: () {
                   /////go to camera
-              },
-            ),
-            
-          ],
-        );
-
-final topAppBarCart = new AppBar(
-          title: new Column(
-            children: <Widget>[
-              Image(image: new AssetImage('images/full_logo.png')),
-              Text(
-              r"Your total is $235.23",
-                style: new TextStyle(fontSize:30.0,
-                color: const Color(0xFF080808),
-                fontWeight: FontWeight.w600,
-                fontFamily: "Roboto"),
-              ),
-            ],
-          ), 
-          actions: <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.camera_alt),
-              tooltip: 'QR scan',
-              onPressed: () {
               },
             ),
             
