b0b64a91882dda96ccf77f9de7e76f5e2b94ad54
tadowdy <tadowdy@siue.edu>
2018-11-01 21:38:30 -0500

    start creating constants for text, padding, etc,

--- pbr/lib/Auxiliary/uiComponents.dart
+++ pbr/lib/Auxiliary/uiComponents.dart
@@ -5,6 +5,20 @@ final themeColor = Colors.blueGrey[300];
 final appBarColor = themeColor;
 final iconColor = Colors.black38;
 final iconSelectedColor = Colors.black;
+final font = "Roboto";
+
+// number constants must be declared const
+// padding constants
+const topPadding = 15.0;
+const horizontalPadding = 5.0;
+const imgWidth = 400.0;
+const imgHeight = 500.0;
+
+// text constants
+const headingOne = 25.0;
+const headingTwo = 20.0;
+const headingThree = 15.0;
+const contentTextSize = 10.0;
 
 // static components
 final topAppBar = new AppBar(
@@ -15,7 +29,7 @@ final topAppBar = new AppBar(
       icon: new Icon(Icons.camera_alt),
       tooltip: 'QR scan',
       onPressed: () {
-        /////go to camera
+        //TODO: go to camera
       },
     ),
   ],
