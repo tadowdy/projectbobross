a1b7ec86c2811dc8addc6d05c8b9ecd8fb7db98f
Logan <lwaldro@siue.edu>
2019-02-21 19:21:28 -0600

    clean event details page: add icon, add title, add date, add price, add description, add buy btn.

--- jacoby_arts/lib/pages/EventDetailsPage.dart
+++ jacoby_arts/lib/pages/EventDetailsPage.dart
@@ -21,15 +21,137 @@ class EventDetailsPage extends StatelessWidget {
 }
 
 buildBody(BuildContext context, eventData){
+
+    //final levelIndicator = Container(
+    //child: Container(
+      //child:LinearProgressIndicator(
+        //backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+        //value: eventData.price,
+        //valueColor:AlwaysStoppedAnimation(Colors.green)),
+      //),
+    //);
+
+      final eventPrice = Container(
+    padding: const EdgeInsets.all(7.0),
+    decoration: new BoxDecoration(
+      border: new Border.all(color: Colors.white),
+      borderRadius: BorderRadius.circular(5.0)),
+      child: new Text(
+        "\$" + eventData.price.toString(),
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+
+    final topContentText = Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      SizedBox(height:40.0),
+      Icon(
+        Icons.calendar_today,
+        color: Colors.white,
+        size:40.0,
+      ),
+      Container(
+        width: 120.0,
+        
+        child: new Divider(color:Colors.yellow),
+      ),
+      SizedBox(height: 10.0),
+      Text(
+        eventData.title,
+        style: TextStyle(color: Colors.white, fontSize: 45.0),
+      ),
+      SizedBox(height: 10.0),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          //Expanded(flex: 1, child: levelIndicator),
+          Expanded(
+            flex: 6,
+            child: Padding(
+              padding:EdgeInsets.only(left: 10.0),
+              child: Text(
+                eventData.date.toString(),
+                style: TextStyle(color: Colors.white),
+              )
+            )
+          ),
+          Expanded(flex: 1, child: eventPrice)
+        ],
+      ),
+    ],
+  );
+  
+  final topContent = Stack(
+  children: <Widget>[
+    Container(
+      padding: EdgeInsets.only(left:10.0),
+      height: MediaQuery.of(context).size.height * 0.4,
+      decoration: new BoxDecoration(
+        image: new DecorationImage(
+          image: new AssetImage('images/lobby.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      ),
+    Container(
+      height: MediaQuery.of(context).size.height * 0.4,
+      padding: EdgeInsets.all(40.0),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(color: Color.fromRGBO(58,66,86,.9)),
+      child: Center(
+        child: topContentText,
+    ),
+    ),
+    Positioned(
+      left:8.0,
+      top: 60.0,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+        },
+      )
+    )
+  ],);
+
+  
+    final bottomContentText = Text(
+      eventData.description,
+      style: TextStyle(fontSize: 18.0),
+    );
+
+    final readButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child:  RaisedButton(
+        onPressed: () => {},
+        color: Color.fromRGBO(58, 66, 86, 1.0),
+        child:
+          Text("BUY TICKETS", style: TextStyle(color: Colors.white)),
+      )
+    );
+
+    final bottomContent = Container(
+      //height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      //color: Theme.of(context).primaryColor,
+      padding: EdgeInsets.all(40.0),
+      child: Center(
+        child: Column(
+          children: <Widget>[bottomContentText,readButton],
+        ),
+      ),
+    );
+
+
   return new Scaffold(
           appBar: new AppBar(
           title: new Text(eventData.title)
           ),
-    body: new Stack(fit: StackFit.expand, children: [
-  addTimeDate(context, eventData),
-  addDescription(context,eventData),
-]));
-}
+    body: Column(
+      children: <Widget>[topContent, bottomContent]
+    ),
+  );
+
 addTimeDate(BuildContext context, eventData){
   return new SingleChildScrollView(
   child: new Container(
@@ -37,17 +159,23 @@ addTimeDate(BuildContext context, eventData){
         top: topPadding, left: horizontalPadding, right: horizontalPadding, bottom: verticalWidgetPadding),
     child: new Row(
       children: <Widget>[
-        new Expanded(child: new Text(eventData.location, style: headingOneBold)),
+        
+        new Column(children: <Widget>[
+        new Text(eventData.location, style: headingOne),
         new Text(
           eventData.date,
-          style: headingOneBold,
+          style: headingOne,
         ),
+        ],
+        ),
+        
       ],
     ),
   ),
 );
 }
 addDescription(BuildContext context, eventData){
+
    return new SingleChildScrollView(
     child: new Container(
         margin: const EdgeInsets.only(
@@ -87,4 +215,7 @@ addDescription(BuildContext context, eventData){
             ),
           ),
         ])));
+}
+
+
 }
