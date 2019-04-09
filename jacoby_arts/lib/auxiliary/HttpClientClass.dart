00c931f04a37c70c7521685f6029b2098287d3c1
Justin <jubruce@siue.edu>
2019-04-09 13:18:47 -0500

    for thomas

--- jacoby_arts/lib/auxiliary/HttpClientClass.dart
+++ jacoby_arts/lib/auxiliary/HttpClientClass.dart
@@ -1,39 +1,21 @@
+import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:jacoby_arts/auxiliary/file';
 
-// getImage(imageName)async{
-// print('here');
-// final FirebaseStorage storage = FirebaseStorage(
-//   app: Firestore.instance.app,
-//   storageBucket: 'gs://jacobyartscenter-7657c.appspot.com'
-// );
-// var image = await storage.ref().child(imageName);
-// //var ref = FirebaseStorage.instance.ref().child(imageName);
-// print('here1');
-// //var location = await ref.getDownloadURL() as String;
+getImage(imageName)async{
+print('here');
+ final StorageReference ref1 = FirebaseStorage.instance.ref();
+ final StorageReference ref = FirebaseStorage.instance.ref().child(imageName);
+ var path = await ref1.getPath();
+print('here1');
+ var image = await ref.getDownloadURL();
+ print(path);
+ print(image);
 
 
-
-// return await image;
-  Future getImageUrl() async {
-    if (imageUrl != null) {
-      return;
-    }
-    // Get an Image
-    HttpClient http = new HttpClient();
-    try {
-      var uri = new Uri.http('dog.ceo', '/api/breeds/image/random');
-      var request = await http.getUrl(uri);
-      var response = await request.close();
-      var responseBody = await response.transform(utf8.decoder).join();
-      var decoded = json.decode(responseBody);
-      var url = decoded['message'];
-      imageUrl = url;
-    } catch (exception) {
-      print(exception);
-    }
-  }
+}
