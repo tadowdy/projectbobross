872d3f724436f3465aa72318338404d57f192baf
tadowdy <tadowdy@siue.edu>
2019-04-10 17:02:34 -0500

    Revert "make body scrollable by changing it to a ListView"
    This reverts commit 33d3e644c42032d9b2a4b23b52f5dbcc8a20507a.

--- jacoby_arts/lib/widgets/CartPageBody.dart
+++ jacoby_arts/lib/widgets/CartPageBody.dart
@@ -29,7 +29,7 @@ class _CartPageBody extends State<CartPageBody> {
   }
 
   Widget makeBody(BuildContext context, List<CartItemInfo> _cartItems) {
-    return new ListView(
+    return new Column(
       children: <Widget>[
         _cartHeader(context, _cartItems),
         _buildCartItems(_cartItems),
