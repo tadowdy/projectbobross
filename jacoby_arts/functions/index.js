ff0b043c28e81f7832b10f76eadf8843db7abc06
tadowdy <tadowdy@siue.edu>
2019-04-13 17:08:08 -0500

    add access to functions

--- jacoby_arts/functions/index.js
+++ jacoby_arts/functions/index.js
@@ -1,6 +1,8 @@
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+import * as functions from 'firebase-functions';
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
